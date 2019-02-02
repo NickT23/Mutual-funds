c3feda98c8cc0a0e72ff9ca6e01270740be8a86d
ashwanig3 <goswami17829@gmail.com>
2019-02-02 11:25:59 +0530

    refactor

--- client/src/components/FundsCategory.js
+++ client/src/components/FundsCategory.js
@@ -1,63 +1,69 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Invest from './Invest';
-import Suggestion from './Suggestion';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Invest from "./Invest";
+import Suggestion from "./Suggestion";
 
 class FundsCategory extends Component {
-    state = {
-        wantToInvest: false
-    }
-    handleInvestment = () => {
-        this.setState({
-            wantToInvest: !this.state.wantToInvest
-        })
-    }
+  state = {
+    wantToInvest: false
+  };
+  //handleInvestment
+  handleInvestment = () => {
+    this.setState({
+      wantToInvest: !this.state.wantToInvest
+    });
+  };
   render() {
-      const { funds }= this.props;
-      if(this.state.wantToInvest) return <Invest handleRemove={this.handleInvestment} />
-     else return (
-            <div className="funds-category">
-              <header className="header">
-                <img src="/images/logo.png" className="logo-img" alt="logo" />
-                <button className="invest-btn" onClick={this.handleInvestment}>Want To Invest?</button>
-              </header>
-              {
-                 funds.length ? 
-                 <div className="funds-history">
-                     <h2>Top Performing {funds[0].type}</h2>
-                    <ol>
-                        <div className="fund-heading">
-                        <span>Funds</span>
-                        <span>5yr returns</span>
-                        </div>
-                    {
-                    funds && funds.map(fund => 
-                        <li className="fund-item">
-                            <span>{fund.name}</span>
-                            <span>{fund.fiveYrReturns}%</span>
-                        </li>
-                        )
-                    }
-                     </ol> 
-
-                 </div> : 
-                 <div className="hero">
-                    <h1>Mutual Fund Recommendation.</h1>
-                    <p>Find where and how much should you invest.</p>
-                    <button className="invest-btn btn-center" onClick={this.handleInvestment}>Get Started</button>
-                 </div>
-              }
-             
+    const { funds } = this.props;
+    if (this.state.wantToInvest)
+      return <Invest handleRemove={this.handleInvestment} />;
+    else
+      return (
+        <div className="funds-category">
+          <header className="header">
+            <img src="/images/logo.png" className="logo-img" alt="logo" />
+            <button className="invest-btn" onClick={this.handleInvestment}>
+              Want To Invest?
+            </button>
+          </header>
+          {funds.length ? (
+            <div className="funds-history">
+              <h2>Top Performing {funds[0].type}</h2>
+              <ol>
+                <div className="fund-heading">
+                  <span>Funds</span>
+                  <span>5yr returns</span>
+                </div>
+                {funds &&
+                  funds.map(fund => (
+                    <li className="fund-item">
+                      <span>{fund.name}</span>
+                      <span>{fund.fiveYrReturns}%</span>
+                    </li>
+                  ))}
+              </ol>
             </div>
-          )
-
-       }
+          ) : (
+            <div className="hero">
+              <h1>Mutual Fund Recommendation.</h1>
+              <p>Find where and how much should you invest.</p>
+              <button
+                className="invest-btn btn-center"
+                onClick={this.handleInvestment}
+              >
+                Get Started
+              </button>
+            </div>
+          )}
+        </div>
+      );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        splittedAmount: state.splittedAmount
-    }
-}
+const mapStateToProps = state => {
+  return {
+    splittedAmount: state.splittedAmount
+  };
+};
 
 export default connect(mapStateToProps)(FundsCategory);
