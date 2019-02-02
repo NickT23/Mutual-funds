aea6d64d6fc8e90fe08b1b2bf206125f7d7f4a42
ashwanig3 <goswami17829@gmail.com>
2019-02-02 11:18:34 +0530

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,61 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
-import Mfunds from './components/Mfunds';
-import FundsCategory from './components/FundsCategory';
-import { getAllBalancedFunds, getAllElssFunds, getAllEquityFunds, getAllDebtFunds, getAllSmallMidcapFunds } from './actions/actionCreator';
-import Suggestion from './components/Suggestion';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Mfunds from "./components/Mfunds";
+import FundsCategory from "./components/FundsCategory";
+import {
+  getAllBalancedFunds,
+  getAllElssFunds,
+  getAllEquityFunds,
+  getAllDebtFunds,
+  getAllSmallMidcapFunds
+} from "./actions/actionCreator";
+import Suggestion from "./components/Suggestion";
 
-const url = process.env.NODE_ENV == 'production' ?  'http://cleartax-mf-recommendation.herokuapp.com/api' : 'http://localhost:8000/api'
+const url =
+  process.env.NODE_ENV == "production"
+    ? "http://cleartax-mf-recommendation.herokuapp.com/api"
+    : "http://localhost:8000/api";
 
 class App extends Component {
   state = {
     targetFund: []
-  }
+  };
   componentDidMount = () => {
-    this.props.dispatch(getAllBalancedFunds())
-    this.props.dispatch(getAllElssFunds())
-    this.props.dispatch(getAllEquityFunds())
-    this.props.dispatch(getAllDebtFunds())
-    this.props.dispatch(getAllSmallMidcapFunds())
-  }
-
-  handleTargetFund = (fund) => {
+    this.props.dispatch(getAllBalancedFunds());
+    this.props.dispatch(getAllElssFunds());
+    this.props.dispatch(getAllEquityFunds());
+    this.props.dispatch(getAllDebtFunds());
+    this.props.dispatch(getAllSmallMidcapFunds());
+  };
+  //handleTargetFund function
+  handleTargetFund = fund => {
     fetch(`${url}/${fund}`)
-    .then(res => res.json())
-    .then(data => {
-      this.setState({
-        targetFund: data.data
-      })
-    })
-  }
-  
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          targetFund: data.data
+        });
+      });
+  };
+
   render() {
     return (
-        <BrowserRouter>
-          <div className="App">
+      <BrowserRouter>
+        <div className="App">
           <Mfunds handleTarget={this.handleTargetFund} />
           <FundsCategory funds={this.state.targetFund} />
           {/* <Suggestion /> */}
-          
         </div>
       </BrowserRouter>
-      
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     splittedAmount: state.splittedAmount
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps)(App);
